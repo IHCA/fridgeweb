8cbbe117ca79b6e61e87b320ec8d09bf7e10a9f2
IHCA <83703795+IHCA@users.noreply.github.com>
2023-08-20 23:34:11 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,6 @@ Route::middleware('auth')->group(function () {
     Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('temp', [TempController::class, 'edit'])->name('temp.tmp');
-    Route::post('temp', [TempController::class, 'store']);
     Route::get('add', [AddFridgeController::class, 'edit'])->name('add.fridge');
     Route::post('add', [AddFridgeController::class, 'store']);
     Route::get('show', [ShowController::class, 'edit'])->name('show.show-fridge');
