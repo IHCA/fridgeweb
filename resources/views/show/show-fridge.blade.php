493b8e55939a18aebd3e55c455331d80dbae5512
IHCA <83703795+IHCA@users.noreply.github.com>
2023-09-10 00:20:31 +0800

    Update show-fridge.blade.php

--- resources/views/show/show-fridge.blade.php
+++ resources/views/show/show-fridge.blade.php
@@ -7,22 +7,34 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                @foreach ($userFridges as $fridge)
+            @foreach ($userFridges as $fridge)
                 <div class="py-6">
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900">
-                            <p><strong>Name:</strong> {{ $fridge->name }}</p>
-                            <p><strong>City/County:</strong> {{ $fridge->city }}</p>
-                            <p><strong>District:</strong> {{ $fridge->dist }}</p>
-                            <p><strong>Address:</strong> {{ $fridge->address }}</p>
-                            <p><strong>Company:</strong> {{ $fridge->company }}</p>
-                            <p><strong>Telephone:</strong> {{ $fridge->telephone }}</p>
-                            <p><strong>Email:</strong> {{ $fridge->email }}</p>
+                        <p style="margin-bottom: 10px;"><strong>Name:</strong> {{ $fridge->name }}</p>
+                            <p style="margin-bottom: 10px;"><strong>City/County:</strong> {{ $fridge->city }}</p>
+                            <p style="margin-bottom: 10px;"><strong>District:</strong> {{ $fridge->dist }}</p>
+                            <p style="margin-bottom: 10px;"><strong>Address:</strong> {{ $fridge->address }}</p>
+                            <p style="margin-bottom: 10px;"><strong>Company:</strong> {{ $fridge->company }}</p>
+                            <p style="margin-bottom: 10px;"><strong>Telephone:</strong> {{ $fridge->telephone }}</p>
+                            <p style="margin-bottom: 10px;"><strong>Email:</strong> {{ $fridge->email }}</p>
+
+                            <!-- Edit and Delete buttons with increased spacing -->
+                            <div class="flex items-center gap-4">
+                                <!-- Edit button -->
+                                <a href="/show/edit/{{ $fridge->id }}" class="btn btn-primary" style="background-color: black; color: white; border-radius: 10px; padding: 6px 12px; font-size: 16px;">編輯</a>
+
+                                <!-- Delete button with margin -->
+                                <form method="POST" action="{{ route('fridgeedit.delete', ['fridgeId' => $fridge->id]) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger" style="background-color: red; color: white; border-radius: 10px; padding: 6px 12px; font-size: 16px; margin-left: 10px;" onclick="return confirm('確定要刪除這個冰箱嗎？')">刪除</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-                @endforeach
+            @endforeach
         </div>
     </div>
-
 </x-app-layout>
